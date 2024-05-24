150c95111457d344846ac6786abad145fb72a1ff
ray <raypang@tuta.io>
2024-05-24 21:49:33 +0800

    Add params check

--- pyade/analysis/__init__.py
+++ pyade/analysis/__init__.py
@@ -24,17 +24,21 @@ class FeeCalculable(Protocol):
     def get_swap_fee(symstr:SymbolStr, lot:Decimal, from_sec:float, to_sec:float, is_buy:bool) -> Decimal:
         raise NotImplementedError()
 
-def get_emas(num_list: list[Decimal], win:int) -> list[Decimal]:
-    ema_series = pd.Series(num_list).ewm(span=win, adjust=False).mean()
+def get_emas(nums: list[Decimal], win:int) -> list[Decimal]:
+    if not nums:
+        return []
+    ema_series = pd.Series(nums).ewm(span=win, adjust=False).mean()
     ema_float_list = list(ema_series)
     ema_decimal_list = [Decimal(ema) for ema in ema_float_list]
     return ema_decimal_list
 
-def get_atrs(h_list: list[Decimal], l_list: list[Decimal], c_list: list[Decimal], win=int) -> list[Decimal]:
+def get_atrs(highs: list[Decimal], lows: list[Decimal], closes: list[Decimal], win=int) -> list[Decimal]:
+    if not highs or not lows or not closes:
+        return []
     df = pd.DataFrame(dict(
-        h=h_list,
-        l=l_list,
-        c=c_list
+        h=highs,
+        l=lows,
+        c=closes
     ))
     df['h-l'] = abs(df.h - df.l)
     df['h-prev_c'] = abs(df.h - df.c.shift())
@@ -44,8 +48,10 @@ def get_atrs(h_list: list[Decimal], l_list: list[Decimal], c_list: list[Decimal]
     atr_decimal_list = [Decimal(atr) for atr in atr_float_list]
     return atr_decimal_list
 
-def get_rsis(num_list:list[Decimal], win:int) -> list[Decimal]:
-    close_delta = pd.Series(num_list).diff()
+def get_rsis(nums:list[Decimal], win:int) -> list[Decimal]:
+    if not nums:
+        return []
+    close_delta = pd.Series(nums).diff()
     up = close_delta.clip(lower=0)
     down = -1 * close_delta.clip(upper=0)
 
@@ -56,21 +62,23 @@ def get_rsis(num_list:list[Decimal], win:int) -> list[Decimal]:
     rsi_decimal_list = [Decimal(rsi) for rsi in rsi_float_list]
     return rsi_decimal_list
 
-def get_peaks_valleys(candle_list: list[Candle]) -> tuple[set[Candle], set[Candle]]:
+def get_peaks_valleys(candles: list[Candle]) -> tuple[set[Candle], set[Candle]]:
+    if not candles:
+        return set(), set()
     # peaks and valleys
     peak_set = set()
     valley_set = set()
-    for i in range(1,len(candle_list)-1):
-        candle = candle_list[i]
-        prev_candle = candle_list[i - 1]
-        next_candle = candle_list[i + 1]
+    for i in range(1,len(candles)-1):
+        candle = candles[i]
+        prev_candle = candles[i - 1]
+        next_candle = candles[i + 1]
         if candle.c > max(prev_candle.c, next_candle.c):
             peak_set.add(candle)
         elif candle.c < min(prev_candle.c, next_candle.c):
             valley_set.add(candle)
 
     # calc retracement
-    peak_valley_list = [c for c in candle_list if c in {*peak_set, *valley_set}]
+    peak_valley_list = [c for c in candles if c in {*peak_set, *valley_set}]
     for i in range(3, len(peak_valley_list)):
         candle = peak_valley_list[i]
         prev_candle = peak_valley_list[i - 1]
@@ -87,29 +95,31 @@ def get_peaks_valleys(candle_list: list[Candle]) -> tuple[set[Candle], set[Candl
                 valley_set.remove(candle)
     return peak_set, valley_set
 
-def get_signals(candle_list: list[Candle],
-                buy_set: set[Candle],
-                sell_set: set[Candle]) -> list['Signal']:
+def get_signals(candles: list[Candle],
+                buys: set[Candle],
+                sells: set[Candle]) -> list['Signal']:
+    if not candles or not buys or not sells:
+        return []
     '''
     因为sl和tp的逻辑，对策略的成功率也是有很大影响，所以构造 signal的方法，每个策略都应该独立，放在对应策略的内部
     '''
     # compose signals
-    atr_list = get_atrs([c.h for c in candle_list],
-                         [c.l for c in candle_list],
-                         [c.c for c in candle_list],
+    atr_list = get_atrs([c.h for c in candles],
+                         [c.l for c in candles],
+                         [c.c for c in candles],
                          win=14)
-    atr_dict = {c: atr_list[i] for i, c in enumerate(candle_list)}
+    atr_dict = {c: atr_list[i] for i, c in enumerate(candles)}
 
     signal_list = []
-    for i, candle in enumerate(candle_list):
-        prev_candle = candle_list[i - 1] if (i - 1) >= 0 else None
+    for i, candle in enumerate(candles):
+        prev_candle = candles[i - 1] if (i - 1) >= 0 else None
         if not prev_candle:
             continue
-        if not (candle in {*buy_set, *sell_set}):
+        if not (candle in {*buys, *sells}):
             continue
         price = candle.c
         atr = atr_dict[candle]
-        if candle in buy_set:
+        if candle in buys:
             min_l = min(candle.l, prev_candle.l)
             sl = min_l - atr
             is_buy = True
@@ -129,30 +139,31 @@ def get_signals(candle_list: list[Candle],
         signal_list.append(signal)
     return signal_list
 
-def get_me_to_prev_valley(candle_list: list[Candle],
-                      me_idx: int,
-                      valley_set:set[Candle]) -> list[Candle]:
-    me = candle_list[me_idx]
+def get_me_to_prev_valley(candles: list[Candle],
+                            me_idx: int,
+                            valleys:set[Candle]) -> list[Candle]:
+    
+    me = candles[me_idx]
     up_trend_list = [me]
     i = me_idx - 1
     while i >= 0:
-        candle = candle_list[i]
+        candle = candles[i]
         up_trend_list.append(candle)
-        if candle in valley_set:
+        if candle in valleys:
             break
         i -= 1
     return up_trend_list
 
-def get_me_to_prev_peak(candle_list: list[Candle],
+def get_me_to_prev_peak(candles: list[Candle],
                     me_idx: int,
-                    peak_set:set[Candle]) -> list[Candle]:
-    me = candle_list[me_idx]
+                    peaks:set[Candle]) -> list[Candle]:
+    me = candles[me_idx]
     down_trend_list = [me] #not put me inside loop to check peak, it me is peak already, will break instantly
     i = me_idx - 1
     while i >= 0:
-        candle = candle_list[i]
+        candle = candles[i]
         down_trend_list.append(candle)
-        if candle in peak_set:
+        if candle in peaks:
             break
         i -= 1
     return down_trend_list
